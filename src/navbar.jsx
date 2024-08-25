ea79b841cbec20f485d03b9fca9bf85cb6801154
Sameer Baker <132022278+sameerbaker@users.noreply.github.com>
2024-08-25 14:06:42 +0300

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,7 +2,9 @@ export default function Navbar(){
     return(
         <nav>
         <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a></nav>
+        <a href="#about">clients</a>
+        <a href="#contact">employees</a>
+        <a href="#contact">footer</a>
+        </nav>
     )
 };
